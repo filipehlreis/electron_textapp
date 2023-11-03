5612dbb4429b8e140ea7ec8ed776156a4d1faad1
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2023-11-03 19:26:49 -0300

    chore: install prettier and eslint for the project

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ const electron = require('electron');
 const { app, BrowserWindow } = electron;
 
 app.on('ready', () => {
-    let win = new BrowserWindow({});
-    win.loadFile('index.html');
-})
+  let win = new BrowserWindow({});
+  win.loadFile('index.html');
+});
