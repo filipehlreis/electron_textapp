5749314cc4425c36f90cd4bff0db7a2f633ad8c8
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2023-11-02 23:57:55 -0300

    feat(aula10): adding our renderer process, loadFile index.html with some text in it

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const electron = require('electron');
-const { app } = electron;
+const { app, BrowserWindow } = electron;
 
 app.on('ready', () => {
-    console.log('app is ready');
+    let win = new BrowserWindow({});
+    win.loadFile('index.html');
 })
